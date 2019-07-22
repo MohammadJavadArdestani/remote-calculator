0251aba5fc3ad7b65f8d8bbb71b7af110dbaf4e6
mohammadjavadardestani <mjavad.ardestani00@gmail.com>
2019-07-22 13:52:04 +0430

    edited

--- client/client.go
+++ client/client.go
@@ -7,45 +7,14 @@ import (
 	"os"
 )
 
-/*func SplitAny(s string, seps string) []string {
-	splitter := func(r rune) bool {
-		return strings.ContainsRune(seps, r)
-	}
-
-	return strings.FieldsFunc(s, splitter)
-}*/
-
 func RunClient() {
-	/*reader:=bufio.NewReader(os.Stdin)
-	clientInput , _:=reader.ReadString ('\n')
 
-	s := SplitAny(clientInput,"+-")
-
-	ip, port := s[0], s[1]
-
-	fmt.Print(ip+"annnnd"+port)
-	fmt.Print("saaalamm")*/
-
-
-		// connect to this socket
-		conn,_ :=net.Dial("tcp","127.0.0.1:5040")
-		fmt.Println("please your import Computational term whit one operation and no white space: ")
-		reader:=bufio.NewReader(os.Stdin)
-		input,_:=reader.ReadString('\n')
-		fmt.Fprint(conn,input)
-		messageRecive,_:=bufio.NewReader(conn).ReadString('\n')
-		fmt.Println("server answer:",messageRecive)
-
-		/*conn, _ := net.Dial("tcp", "127.0.0.1:8081")
-		for {
-			// read in input from stdin
-			reader := bufio.NewReader(os.Stdin)
-			fmt.Print("Text to send: ")
-			text, _ := reader.ReadString('\n')
-			// send to socket
-			fmt.Fprintf(conn, text + "\n")
-			// listen for reply
-			message, _ := bufio.NewReader(conn).ReadString('\n')
-			fmt.Print("Message from server: "+message)*/
+	conn, _ := net.Dial("tcp", "127.0.0.1:5040")
+	fmt.Println("please your import Computational term whit one operation and no white space: ")
+	reader := bufio.NewReader(os.Stdin)
+	input, _ := reader.ReadString('\n')
+	fmt.Fprint(conn, input)
+	messageRecive, _ := bufio.NewReader(conn).ReadString('\n')
+	fmt.Println("server answer:", messageRecive)
 
 }
--- server/server.go
+++ server/server.go
@@ -8,42 +8,19 @@ import (
 )
 
 func RunServer() {
- fmt.Println("server is running")
+	fmt.Println("server is running")
 	client, err := net.Listen("tcp", ":5040")
-	if err !=nil{
+	if err != nil {
 		fmt.Println("1")
 	}
 	connection, err := client.Accept()
-	if err !=nil {
+	if err != nil {
 		fmt.Println("2")
 	}
 	recivedMessage, _ := bufio.NewReader(connection).ReadString('\n')
-	result:= Calculate(recivedMessage)
-	//fmt.Println(result)
-	//fmt.Println("its showing on server:",result)
-	//result := strings.ToLower(recivedMessage)
-	answer:=strconv.FormatFloat(result,'f',-1,64)
+	result := Calculate(recivedMessage)
+
+	answer := strconv.FormatFloat(result, 'f', -1, 64)
 
 	_, _ = fmt.Fprint(connection, answer+"\n")
 }
-	/*fmt.Println("Launching server...")
-
-	// listen on all interfaces
-	ln, _ := net.Listen("tcp", ":8081")
-
-	// accept connection on port
-	conn, _ := ln.Accept()
-
-	// run loop forever (or until ctrl-c)
-	for {
-		// will listen for message to process ending in newline (\n)
-		message, _ := bufio.NewReader(conn).ReadString('\n')
-		// output message received
-		fmt.Print("Message Received:", string(message))
-		// sample process for string received
-		newmessage := strings.ToUpper(message)
-		// send new string back to client
-		conn.Write([]byte(newmessage + "\n"))*/
-
-
-
